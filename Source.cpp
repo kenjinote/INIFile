698a9a984763a90adfc104bad585c1333b946d7f
kenjinote <kenji256@gmail.com>
2017-10-30 14:17:55 +0900

    Update Source.cpp

--- Source.cpp
+++ Source.cpp
@@ -1,6 +1,6 @@
-﻿#pragma comment(linker,"\"/manifestdependency:type='win32' name='Microsoft.Windows.Common-Controls' version='6.0.0.0' processorArchitecture='*' publicKeyToken='6595b64144ccf1df' language='*'\"")
+﻿#pragma comment(linker, "\"/manifestdependency:type='win32' name='Microsoft.Windows.Common-Controls' version='6.0.0.0' processorArchitecture='*' publicKeyToken='6595b64144ccf1df' language='*'\"")
 
-#pragma comment(lib,"shlwapi")
+#pragma comment(lib, "shlwapi")
 
 #include <windows.h>
 #include <shlwapi.h>
